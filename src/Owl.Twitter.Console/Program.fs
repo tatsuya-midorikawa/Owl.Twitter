ba1b8489b0e5554015a8f3b2663c15644e41b8d5
midoliy <midoliy@outlook.jp>
2021-11-26 00:20:13 +0900

    add sample

--- src/Owl.Twitter.Console/Program.fs
+++ src/Owl.Twitter.Console/Program.fs
@@ -1,1 +1,23 @@
-﻿open System
+﻿open System.Net.Http
+
+let toAsync = Async.AwaitTask
+let run = Async.RunSynchronously
+
+[<Literal>]
+let token = ""
+
+[<EntryPoint>]
+let main _ =
+  use client = new HttpClient()
+  use request = new HttpRequestMessage(HttpMethod.Get, "https://api.twitter.com/2/tweets/search/recent?max_results=10&query=from:Twitter")
+  request.Headers.Add("ContentType", "application/json")
+  request.Headers.Add("Authorization", $"Bearer %s{token}")
+
+  task {
+    use! r = client.SendAsync(request)
+    return! r.Content.ReadAsStringAsync()
+  }
+  |> (toAsync >> run)
+  |> printfn "%s"
+  
+  0
